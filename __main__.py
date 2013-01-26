14aefbe3dd295795825bd294e84211463463bb90
Creack <charmes.guillaume@gmail.com>
2013-01-25 21:44:48 -0500

    Dump the account data in web

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@
 Main application file
 """
 
-from AppIB import AppIB
+from AppIB import AppIB, db
 from sys import exit
 from flask import Flask
 #from Strategy import Strategy
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "<br />\n".join([str(k) for k in db.account.find()])
 
 if __name__ == "__main__":
     try:
